258fb43c1a2fd990675a311875db2a76ea815a62
Joshua-Riley-ZD <joshua.riley@ziffmedia.com>
2026-07-15 11:40:41 +0200

    Upgrade Astro to version 7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://rosarioleonardi.shop',
   integrations: [
-    tailwind(),
     alpinejs({ entrypoint: '/src/entrypoint' }),
     sitemap(),
   ],
